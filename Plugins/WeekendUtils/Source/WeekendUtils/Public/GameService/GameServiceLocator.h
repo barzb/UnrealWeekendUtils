ab45d23484323e713e54fafe514a54ea33be6502
barzb <barzbenni@gmail.com>
2023-04-27 14:08:22 +0200

    Removed deprecated ServiceLocator and DependencyInjection framework

--- Plugins/WeekendUtils/Source/WeekendUtils/Public/GameService/GameServiceLocator.h
+++ Plugins/WeekendUtils/Source/WeekendUtils/Public/GameService/GameServiceLocator.h
@@ -1,4 +1,11 @@
-﻿// (c) 2023 Nine Worlds Studios GmbH. All rights reserved.
+﻿///////////////////////////////////////////////////////////////////////////////////////
+/// Copyright (C) 2023 by Benjamin Barz in cooperation with Nine Worlds Studios GmbH.
+///
+/// This file is part of the WeekendUtils UE5 Plugin.
+///
+/// Distributed under the MIT License. See accompanying file LICENSE.
+///
+///////////////////////////////////////////////////////////////////////////////////////
 
 #pragma once
 
